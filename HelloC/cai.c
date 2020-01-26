e6d2fac271c4c6876fdf0f28aa855084512a8365
Your Name <you@example.com>
2020-01-26 13:01:30 +0800

    注释

---
+++ HelloC/cai.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+	it const STAUS_
+
+	int a;//用户输入选择
+do{
+   puts("请选择难度等级")；
+   printf("1. 0~9\n 2. 0~99\n 3. 0~999\n 0. 退出\n");
+   printf("你的输入:");scanf("%d"&a);
+}while(a>3||a<0)
+
+printf("你当前输入的难度等级是");
+	switch(a){
+       case 1: printf("0~9");
+       case 2: printf("0~99")；
+       case 3: printf("0~999");
+
+	}
+
+
+
+
+
+	renturn 0;
+}
